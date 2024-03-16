9e04a61516cae05b1975ad7afac6e18ca84da817
6480689 <lasee.len@student.mahidol.edu>
2024-03-17 04:22:23 +0700

    Fix axios

--- src/plugins/vue-axios.js
+++ src/plugins/vue-axios.js
@@ -1,7 +1,7 @@
 import * as Vue from "vue"; // in Vue 3
 import axios from "axios";
 import VueAxios from "vue-axios";
-import App from "./App.vue";
+import App from "../App.vue";
 
 const app = Vue.createApp(App);
 app.use(VueAxios, axios);
