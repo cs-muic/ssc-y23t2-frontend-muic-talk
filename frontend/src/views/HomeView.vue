5b0d563dfb0ac7232fba8bf4b79441f60c22a406
Lothe1 <tath9982@gmail.com>
2024-03-25 14:32:32 +0700

    Added Login page and map /login path and login page in vue router to

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,24 @@
 <template>
-  <hello-world />
+  <v-container>
+    <h2>Home</h2>
+    <ul>
+      <li>
+        <router-link to="/">Go to Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about">Go to AboutView</router-link>
+      </li>
+      <li>
+        <router-link to="/login">Go to LoginView</router-link>
+      </li>
+    </ul>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "Home",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
